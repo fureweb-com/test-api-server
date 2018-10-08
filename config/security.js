21040cc6147ae9672c02911bd796fdc1adaf1f53
fureweb-com <fureweb.com@gmail.com>
2018-10-08 16:04:30 +0900

    수정: cors 설정 변경

--- config/security.js
+++ config/security.js
@@ -28,11 +28,11 @@ module.exports.security = {
   *                                                                          *
   ***************************************************************************/
 
-  // cors: {
-  //   allRoutes: false,
-  //   allowOrigins: '*',
-  //   allowCredentials: false,
-  // },
+  cors: {
+    allRoutes: true,
+    allowOrigins: '*',
+    allowRequestHeaders: 'content-type,x-csrf-token,authorization'
+  },
 
 
   /****************************************************************************
