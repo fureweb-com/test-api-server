052d618088d13889273e1539cb8795129c0cbda3
fureweb-com <fureweb.com@gmail.com>
2018-10-10 00:26:28 +0900

    수정: LoginController 처리로직 임시 추가
    - 타입이 이메일인 경우 비밀번호 확인 추가
    - Social 처리 부분 향후 붙이기

--- api/controllers/LoginController.js
+++ api/controllers/LoginController.js
@@ -1,11 +1,16 @@
 module.exports = {
   async login(req, res) {
-    const { type, id } = req.body
-    if(!type || !id) return res.badRequest()
-
-    // 유효한 사용자인지 확인
+    const { type, id, email, password } = req.body
+    console.log('login called', type, id, email, password)
+    if(!type) return res.badRequest()
 
     // 타입이 이메일인 경우, 내부에 저장된 암호화된 비밀번호와 일치하는지 확인 필요
+    if(type === 'email') {
+      // email, password
+      const isExistUser = UserService.checkPassword(email, password)
+      console.log(isExistUser)
+      if(!isExistUser) return res.badRequest('로그인 실패')
+    }
 
     // 토큰 발급
     // 발급 시점으로부터 30분(기본) 유효한 모든 요청에 사용할 토큰
