45b4b6e3ca851d70b24ad3a310aad5a7647f336a
fureweb-com <fureweb.com@gmail.com>
2018-10-10 00:25:39 +0900

    수정: JoinController 가입 처리 로직 수정
    - 가입 처리 후 로그인 메서드 S2S 처리 변경된 프로토콜에 맞춰 처리

--- api/controllers/JoinController.js
+++ api/controllers/JoinController.js
@@ -4,6 +4,7 @@ const serverHost = process.env.NODE_ENV === 'production' ? 'https://api.odigaji.
 module.exports = {
   async join(req, res) {
     const {email, password1, password2, name, mobile, referrer} = req.body
+    const password = password1
     
     // validation (일단 생략)
     if(!email || !password1 || !password2 || !name)
@@ -14,17 +15,16 @@ module.exports = {
     // 회원 가입 처리 -- 트랜잭션 처리 일단 없음 -_- DB의 도움을 받을 예정.
     const selectedUser = UserService.find('email', email)
     if(selectedUser) return res.badRequest('사용중인 이메일입니다.')
-
-    const currentUser = { id: UserService.nextId(), email, password: password1, name, mobile, referrer }
+    
+    const currentUser = { id: UserService.nextId(), email, password, name, mobile, referrer }
     UserService.save(currentUser)
 
     // 로그인 처리 후 토큰 발행하여 발급된 토큰 가져오기
-    const {status, data: result} = (await axios.post(`${serverHost}/api/v1/login`, {type: 'email', id: currentUser.id}))
+    const {status, data: result} = (await axios.post(`${serverHost}/api/v1/login`, {type: 'email', email, password}))
 
     // 토큰이 발행되지 않은, status가 200이 아닌경우 별도 처리 필요
 
     // 테스트 응답용 데이터
-    delete currentUser.password
-    res.json(Object.assign(result, {user: {...currentUser}}))
+    res.json(Object.assign(result))
   }
 }
