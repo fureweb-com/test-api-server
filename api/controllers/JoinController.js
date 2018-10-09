203ecab0897110f4b51905d255b6a7e6ba2e1b5c
fureweb-com <fureweb.com@gmail.com>
2018-10-10 00:05:00 +0900

    수정: JoinController 수정
    - UserService 사용하도록 임시코드 수정

--- api/controllers/JoinController.js
+++ api/controllers/JoinController.js
@@ -1,5 +1,3 @@
-// 임시로 사용자 데이터베이스를 메모리에 올림.
-const users = []
 const axios = require('axios')
 const serverHost = process.env.NODE_ENV === 'production' ? 'https://api.odigaji.com' : 'http://localhost:3000'
 
@@ -14,12 +12,11 @@ module.exports = {
       return res.badRequest('비밀번호가 서로 일치하지 않습니다.')
 
     // 회원 가입 처리 -- 트랜잭션 처리 일단 없음 -_- DB의 도움을 받을 예정.
-    const currentUser = Object.assign({ id: users.length + 1 }, {email, password: password1, name, mobile, referrer})
+    const selectedUser = UserService.find('email', email)
+    if(selectedUser) return res.badRequest('사용중인 이메일입니다.')
 
-    const isExistEmail = users.find(user => user.email === currentUser.email)
-    if(isExistEmail) return res.badRequest('사용중인 이메일입니다.')
-
-    users.push(currentUser)
+    const currentUser = { id: UserService.nextId(), email, password: password1, name, mobile, referrer }
+    UserService.save(currentUser)
 
     // 로그인 처리 후 토큰 발행하여 발급된 토큰 가져오기
     const {status, data: result} = (await axios.post(`${serverHost}/api/v1/login`, {type: 'email', id: currentUser.id}))
