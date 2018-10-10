50db08ee978bf29c7518ff17a1737bba35716083
fureweb-com <fureweb.com@gmail.com>
2018-10-10 17:07:00 +0900

    수정: SignupController 도우미 함수 추가 및 적용
    - social 회원 확인 (find) 처리는 가라로 되어있음 -_-

--- api/controllers/SignupController.js
+++ api/controllers/SignupController.js
@@ -1,30 +1,56 @@
 const axios = require('axios')
 const serverHost = process.env.NODE_ENV === 'production' ? 'https://api.odigaji.com' : 'http://localhost:3000'
+const isValidSignup = (provider, args) => {
+  if(provider === 'email') {
+    console.log('email check')
+    const {email, password, name} = args
+    return !email || !password || !name ? false : true
+  } else {
+    console.log('!email check')
+    const {id} = args
+    return !id ? false : true
+  }
+}
+
+const getUserByProvider = (provider, args) => {
+  if(provider === 'email') {
+    console.log('getUserByProvider email')
+    const {email} = args
+    if(!email) return null
+    else return UserService.find('email', email)
+  } else {
+    console.log('getUserByProvider !email')
+    const {id} = args
+    if(!id) return null
+    else return UserService.find('id', id)
+  }
+}
 
 module.exports = {
   async signup(req, res) {
-    const {email, password1, password2, name, mobile, referrer} = req.body
-    const password = password1
+    console.log('signup called')
+    const {provider, id, email, password, name, mobile, referrer} = req.body
     
-    // validation (일단 생략)
-    if(!email || !password1 || !password2 || !name)
+    if(!isValidSignup(provider, {id, email, password, name, mobile, referrer})){
       return res.badRequest('필수 정보가 누락되었습니다.')
-    else if(password1 !== password2)
-      return res.badRequest('비밀번호가 서로 일치하지 않습니다.')
+    }
 
     // 회원 가입 처리 -- 트랜잭션 처리 일단 없음 -_- DB의 도움을 받을 예정.
-    const selectedUser = UserService.find('email', email)
-    if(selectedUser) return res.badRequest('사용중인 이메일입니다.')
+    const selectedUser = getUserByProvider(provider, {id, email, password, name, mobile, referrer})
+    
+    // const selectedUser = UserService.find('email', email)
+    if(selectedUser) return res.badRequest('이미 가입된 사용자입니다.')
     
     const currentUser = { id: UserService.nextId(), email, password, name, mobile, referrer }
     UserService.save(currentUser)
 
     // 로그인 처리 후 토큰 발행하여 발급된 토큰 가져오기
-    const {status, data: result} = (await axios.post(`${serverHost}/api/v1/login`, {provider: 'email', email, password}))
+    const {status, data: result} = (await axios.post(`${serverHost}/api/v1/users/login`, {provider, email, password}))
 
     // 토큰이 발행되지 않은, status가 200이 아닌경우 별도 처리 필요
 
     // 테스트 응답용 데이터
     res.json(Object.assign(result))
-  }
+  },
+  
 }
