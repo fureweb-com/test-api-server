d3a3c60c096f2359a927c5848387cee4ee73f6ca
fureweb-com <fureweb.com@gmail.com>
2018-10-15 17:31:51 +0900

    추가: BoardController에 getArticleList 메서드 추가

--- api/controllers/BoardController.js
+++ api/controllers/BoardController.js
@@ -1,6 +1,21 @@
 const moment = require('moment')
 
 module.exports = {
+  getArticleList(req, res) {
+    const [type] = [req.param('type')]
+    if(!type) return res.badRequest('타입이 존재하지 않습니다.')
+
+    const result = {
+      boardList: [
+        { id: 1, type, userId: 1, name: '1번 게시글작성자', title: '1번 게시글 제목', content: '1번 게시글 내용', createdAt: +moment(), updatedAt: +moment() },
+        { id: 2, type, userId: 2, name: '2번 게시글작성자', title: '2번 게시글 제목', content: '2번 게시글 내용', createdAt: +moment(), updatedAt: +moment() },
+        { id: 3, type, userId: 3, name: '3번 게시글작성자', title: '3번 게시글 제목', content: '3번 게시글 내용', createdAt: +moment(), updatedAt: +moment() },
+        { id: 4, type, userId: 4, name: '4번 게시글작성자', title: '4번 게시글 제목', content: '4번 게시글 내용', createdAt: +moment(), updatedAt: +moment() },
+        { id: 5, type, userId: 5, name: '5번 게시글작성자', title: '5번 게시글 제목', content: '5번 게시글 내용', createdAt: +moment(), updatedAt: +moment() },
+      ]
+    }
+    res.json(result)
+  },
   getArticle(req, res) {
     const [type, id] = [req.param('type'), req.param('id')]
     if(!type) return res.badRequest('타입이 존재하지 않습니다.')
