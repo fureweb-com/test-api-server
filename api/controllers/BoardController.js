2548fa16fa18c41d890079906925522f5fde5f43
fureweb-com <fureweb.com@gmail.com>
2018-10-15 18:40:27 +0900

    수정: BoardController Board 객체에 view_count 추가

--- api/controllers/BoardController.js
+++ api/controllers/BoardController.js
@@ -2,21 +2,23 @@ const moment = require('moment')
 
 module.exports = {
   getArticleList(req, res) {
+    console.log('getArticleList')
     const [type] = [req.param('type')]
     if(!type) return res.badRequest('타입이 존재하지 않습니다.')
 
     const result = {
       boardList: [
-        { id: 1, type, userId: 1, name: '1번 게시글작성자', title: '1번 게시글 제목', content: '1번 게시글 내용', createdAt: +moment(), updatedAt: +moment() },
-        { id: 2, type, userId: 2, name: '2번 게시글작성자', title: '2번 게시글 제목', content: '2번 게시글 내용', createdAt: +moment(), updatedAt: +moment() },
-        { id: 3, type, userId: 3, name: '3번 게시글작성자', title: '3번 게시글 제목', content: '3번 게시글 내용', createdAt: +moment(), updatedAt: +moment() },
-        { id: 4, type, userId: 4, name: '4번 게시글작성자', title: '4번 게시글 제목', content: '4번 게시글 내용', createdAt: +moment(), updatedAt: +moment() },
-        { id: 5, type, userId: 5, name: '5번 게시글작성자', title: '5번 게시글 제목', content: '5번 게시글 내용', createdAt: +moment(), updatedAt: +moment() },
+        { id: 1, type, user_id: 1, name: '1번 게시글작성자', title: '1번 게시글 제목', content: '1번 게시글 내용', view_count: 1, created_at: +moment(), updated_at: +moment() },
+        { id: 2, type, user_id: 2, name: '2번 게시글작성자', title: '2번 게시글 제목', content: '2번 게시글 내용', view_count: 1, created_at: +moment(), updated_at: +moment() },
+        { id: 3, type, user_id: 3, name: '3번 게시글작성자', title: '3번 게시글 제목', content: '3번 게시글 내용', view_count: 1, created_at: +moment(), updated_at: +moment() },
+        { id: 4, type, user_id: 4, name: '4번 게시글작성자', title: '4번 게시글 제목', content: '4번 게시글 내용', view_count: 1, created_at: +moment(), updated_at: +moment() },
+        { id: 5, type, user_id: 5, name: '5번 게시글작성자', title: '5번 게시글 제목', content: '5번 게시글 내용', view_count: 1, created_at: +moment(), updated_at: +moment() },
       ]
     }
     res.json(result)
   },
   getArticle(req, res) {
+    console.log('getArticle')
     const [type, id] = [req.param('type'), req.param('id')]
     if(!type) return res.badRequest('타입이 존재하지 않습니다.')
     else if(!id) return res.badRequest('아이디가 존재하지 않습니다.')
@@ -24,12 +26,13 @@ module.exports = {
     const result = {
       type,
       id,
-      userId: 1,
+      user_id: 1,
       name: '1번 게시글작성자',
       title: `${type}에 대한 ${id}번 글 테스트 제목`,
       content: `타입: ${type}, 아이디: ${id}}\n해당 게시글에 대한 본문을 DB에서 조회 후 응답`,
-      createdAt: +moment(),
-      updatedAt: +moment(),
+      view_count: 1,
+      created_at: +moment(),
+      updated_at: +moment(),
 
     }
     res.json(result)
