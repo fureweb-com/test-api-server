3d9159e41f143e696c4f20a18276fd769b8e3ea6
fureweb-com <fureweb.com@gmail.com>
2018-10-15 18:41:05 +0900

    수정: BoardReplyController 응답 시 snake_case 적용

--- api/controllers/BoardReplyController.js
+++ api/controllers/BoardReplyController.js
@@ -5,12 +5,12 @@ module.exports = {
     const [type, boardId, page = 1] = [req.param('type'), req.param('id'), req.param('page')]
     const result = {
       replyList: [
-        // {id, content, userId, createdAt, modifiedAt, isLikeVotted, isUnlikeVotted} 형태로 응답
-        { id: 1, userId: 1, name: '1빠충', content: '1빠', createdAt: +moment(), isLikeVotted: false, isUnlikeVotted: false }, // 현재 사용자가 해당 댓글에 대해 추천 또는 비추천했는지에 대한 데이터
-        { id: 2, userId: 2, name: '2빠충', content: '2빠', createdAt: +moment(), isLikeVotted: false, isUnlikeVotted: false }, // 현재 사용자가 해당 댓글에 대해 추천 또는 비추천했는지에 대한 데이터
-        { id: 3, userId: 3, name: '3빠충', content: '3빠', createdAt: +moment(), isLikeVotted: false, isUnlikeVotted: false }, // 현재 사용자가 해당 댓글에 대해 추천 또는 비추천했는지에 대한 데이터
-        { id: 4, userId: 4, name: '4빠충', content: '4빠', createdAt: +moment(), isLikeVotted: false, isUnlikeVotted: false }, // 현재 사용자가 해당 댓글에 대해 추천 또는 비추천했는지에 대한 데이터
-        { id: 5, userId: 5, name: '5빠충', content: '5빠', createdAt: +moment(), isLikeVotted: false, isUnlikeVotted: false }, // 현재 사용자가 해당 댓글에 대해 추천 또는 비추천했는지에 대한 데이터
+        // {id, content, user_id, created_at, modifiedAt, is_like_votted, is_unlike_votted} 형태로 응답
+        { id: 1, user_id: 1, name: '1빠충', content: '1빠', created_at: +moment(), is_like_votted: false, is_unlike_votted: false }, // 현재 사용자가 해당 댓글에 대해 추천 또는 비추천했는지에 대한 데이터
+        { id: 2, user_id: 2, name: '2빠충', content: '2빠', created_at: +moment(), is_like_votted: false, is_unlike_votted: false }, // 현재 사용자가 해당 댓글에 대해 추천 또는 비추천했는지에 대한 데이터
+        { id: 3, user_id: 3, name: '3빠충', content: '3빠', created_at: +moment(), is_like_votted: false, is_unlike_votted: false }, // 현재 사용자가 해당 댓글에 대해 추천 또는 비추천했는지에 대한 데이터
+        { id: 4, user_id: 4, name: '4빠충', content: '4빠', created_at: +moment(), is_like_votted: false, is_unlike_votted: false }, // 현재 사용자가 해당 댓글에 대해 추천 또는 비추천했는지에 대한 데이터
+        { id: 5, user_id: 5, name: '5빠충', content: '5빠', created_at: +moment(), is_like_votted: false, is_unlike_votted: false }, // 현재 사용자가 해당 댓글에 대해 추천 또는 비추천했는지에 대한 데이터
       ]
     }
 
