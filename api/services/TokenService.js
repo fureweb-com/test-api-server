17de202b4f92da31291583c555e23e0785b7a633
fureweb-com <fureweb.com@gmail.com>
2018-10-16 14:07:00 +0900

    추가: JWT Token decode 처리를 위한 TokenService 작성

---
+++ api/services/TokenService.js
@@ -0,0 +1,36 @@
+const atob = require('atob')
+
+const b64DecodeUnicode = str => {
+  return decodeURIComponent(atob(str).replace(/(.)/g, (m, p) => {
+    let code = p.charCodeAt(0).toString(16).toUpperCase();
+    if (code.length < 2) {
+      code = '0' + code
+    }
+    return '%' + code
+  }))
+}
+
+const getDecodedPayload = str => {
+  let output = str.replace(/-/g, '+').replace(/_/g, '/')
+  switch (output.length % 4) {
+    case 0: break
+    case 2: output += '=='; break
+    case 3: output += '=';  break
+    default: throw 'Illegal base64url string!'
+  }
+
+  try { return b64DecodeUnicode(output) } 
+  catch (err) { return atob(output) }
+}
+
+module.exports = {
+  decodeJWT(jwtToken) {
+    const token = jwtToken.split('.')
+
+    if(token.length !== 3) throw `invalid jwt token: ${jwtToken}`
+    const [,payload,] = token
+    const decodedPayload = getDecodedPayload(payload)
+
+    return JSON.parse(decodedPayload)
+  }
+}
