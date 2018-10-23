5e705343c006807eafa3b437c2d641ee9f688316
fureweb-com <fureweb.com@gmail.com>
2018-10-23 11:59:40 +0900

    추가: BoardReplyController 수정, 삭제 기능 임시 구현
    - 서버 부팅 시 생성되는 임시 배열에 수정 / 삭제처리

--- api/controllers/BoardReplyController.js
+++ api/controllers/BoardReplyController.js
@@ -13,39 +13,53 @@ const extractMandatoryFields = (req, res) => {
   return {category, boardId, replyId, user: userInfo}
 }
 
+// 메모리에 임시로 작성
+const replyList = [
+  // {id, content, user_id, created_at, modifiedAt, is_like_votted, is_unlike_votted} 형태로 응답
+  { id: 1, user_id: 1, name: '1빠충', content: '1빠', created_at: +moment(), is_like_votted: false, is_unlike_votted: false }, // 현재 사용자가 해당 댓글에 대해 추천 또는 비추천했는지에 대한 데이터
+  { id: 2, user_id: 2, name: '2빠충', content: '2빠', created_at: +moment(), is_like_votted: false, is_unlike_votted: false }, // 현재 사용자가 해당 댓글에 대해 추천 또는 비추천했는지에 대한 데이터
+  { id: 3, user_id: 3, name: '3빠충', content: '3빠', created_at: +moment(), is_like_votted: false, is_unlike_votted: false }, // 현재 사용자가 해당 댓글에 대해 추천 또는 비추천했는지에 대한 데이터
+  { id: 4, user_id: 4, name: '4빠충', content: '4빠', created_at: +moment(), is_like_votted: false, is_unlike_votted: false }, // 현재 사용자가 해당 댓글에 대해 추천 또는 비추천했는지에 대한 데이터
+  { id: 5, user_id: 5, name: '5빠충', content: '5빠', created_at: +moment(), is_like_votted: false, is_unlike_votted: false }, // 현재 사용자가 해당 댓글에 대해 추천 또는 비추천했는지에 대한 데이터
+]
+
 module.exports = {
   getReplyList(req, res) {
     const [category, boardId, page = 1] = [req.param('category'), req.param('boardId'), req.param('page')]
-    const result = {
-      replyList: [
-        // {id, content, user_id, created_at, modifiedAt, is_like_votted, is_unlike_votted} 형태로 응답
-        { id: 1, user_id: 1, name: '1빠충', content: '1빠', created_at: +moment(), is_like_votted: false, is_unlike_votted: false }, // 현재 사용자가 해당 댓글에 대해 추천 또는 비추천했는지에 대한 데이터
-        { id: 2, user_id: 2, name: '2빠충', content: '2빠', created_at: +moment(), is_like_votted: false, is_unlike_votted: false }, // 현재 사용자가 해당 댓글에 대해 추천 또는 비추천했는지에 대한 데이터
-        { id: 3, user_id: 3, name: '3빠충', content: '3빠', created_at: +moment(), is_like_votted: false, is_unlike_votted: false }, // 현재 사용자가 해당 댓글에 대해 추천 또는 비추천했는지에 대한 데이터
-        { id: 4, user_id: 4, name: '4빠충', content: '4빠', created_at: +moment(), is_like_votted: false, is_unlike_votted: false }, // 현재 사용자가 해당 댓글에 대해 추천 또는 비추천했는지에 대한 데이터
-        { id: 5, user_id: 5, name: '5빠충', content: '5빠', created_at: +moment(), is_like_votted: false, is_unlike_votted: false }, // 현재 사용자가 해당 댓글에 대해 추천 또는 비추천했는지에 대한 데이터
-      ]
-    }
+    const result = { replyList }
 
     res.json(result)
   },
   updateReply(req, res) {
     const fields = extractMandatoryFields(req, res)
     if(!fields) return res.badRequest()
-
-    const {category, boardId, replyId, user} = fields
-    const {content} = req.body
     
-    console.log('updateReply', category, boardId, replyId, content)
-    res.json({category, boardId, replyId, userId: user.id})
+    const {category, boardId, replyId, user} = fields
+    console.log('updateReply', category, boardId, replyId, user)
+    const {content} = req.body
+    let isUpdated = false
+    replyList.forEach(reply => {
+      if(reply.id === Number(replyId)) {
+        isUpdated = true
+        reply.content = content
+      }
+      return reply
+    })
+
+    res.json({isUpdated})
   },
   deleteReply(req, res) {
     const fields = extractMandatoryFields(req, res)
     if(!fields) return res.badRequest()
 
     const {category, boardId, replyId, user} = fields
-    console.log('deleteReply', category, boardId, replyId)
-    res.json({category, boardId, replyId, userId: user.id})
+    console.log('deleteReply', category, boardId, replyId, user)
+    const indexToDelete = replyList.findIndex(reply => reply.id === Number(replyId))
+    let isDeleted = false
+    if(indexToDelete > -1)
+      isDeleted = !!replyList.splice(indexToDelete, 1).length
+
+    res.json({isDeleted})
   },
   likeReply(req, res) {
     const fields = extractMandatoryFields(req, res)
