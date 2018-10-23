4fe8a797ca46b77d3466ad89d821837270091cb7
fureweb-com <fureweb.com@gmail.com>
2018-10-23 11:01:28 +0900

    추가: 토큰 갱신 시 사용될 TokenController 및 refreshToken 메소드 작성
    - POST /api/v1/token 경로로 body에 refreshToken을 넣어 요청

---
+++ api/controllers/TokenController.js
@@ -0,0 +1,14 @@
+module.exports = {
+  refreshToken(req, res) {
+    // refresh용 token을 전달받는다.
+    const {refreshToken} = req.body
+    if(!refreshToken) return res.badRequest('refreshToken not found')
+
+    console.log('refreshToken request::', refreshToken)
+    // 토큰 새로 발급 후 응답
+    return res.json({
+      token: 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6MSwibmFtZSI6IuqwseyLoOuQnOyCrOyaqeyekCJ9.Ow1rIA5jY2yTFL3CHA8spD_iODBfQBqflPkcfO9Gm-I', 
+      refreshToken: 'new_refresh_token_from_server'
+    })
+  }
+}
--- config/routes.js
+++ config/routes.js
@@ -49,6 +49,9 @@ module.exports.routes = {
   'POST /api/v1/signout' : 'LoginController.logout',
   'POST /api/v1/signup'   : 'SignupController.signup',
 
+  // token refresh
+  'POST /api/v1/token': 'TokenController.refreshToken',
+
   'GET /api/v1/oAuthCallback/:provider' : 'OAuthCallbackController.oAuth',
 
   'GET /api/v1/boards/:category'       : 'BoardController.getArticleList',
