7ea5ed7e3cf8f9a4ca2c56639d52268d76677e1b
fureweb-com <fureweb.com@gmail.com>
2018-11-06 18:22:58 +0900

    수정: BrandController 내 데이터 프로토콜 변경 (camel -> snake)

--- api/controllers/BrandController.js
+++ api/controllers/BrandController.js
@@ -1,12 +1,100 @@
 const brandList = [
-  {id: 1, name: 'Mother ground', simpleDescription: '마더그라운드의 새 엘리먼트입니다. <br>까마귀烏오 WU Series', logoImageUrl: '/images/img-brandlogo-motherground-48.svg', imageUrl:'/images/mother_ground.jpg', isFollow: false, collectionList: [{id: 1, imageUrl:'/images/mother_ground.jpg', isFundingNow: true}, {id: 2, imageUrl:'/images/mother_ground.jpg', isFundingNow: false}]},
-  {id: 2, name: 'Rawrow', simpleDescription: 'Rawrow 관련 브랜드 간단 소개글', logoImageUrl: '/images/img-brandlogo-rawrow-48.svg', imageUrl:'https://via.placeholder.com/1000x1000', isFollow: false, collectionList: [{id: 3, imageUrl:'https://via.placeholder.com/1000x1000', isFundingNow: false}, {id: 4, imageUrl:'https://via.placeholder.com/1000x1000', isFundingNow: false}]},
-  {id: 3, name: 'Hideout', simpleDescription: 'Hideout 관련 브랜드 간단 소개글', logoImageUrl: '/images/img-brandlogo-hideout-48.svg', imageUrl:'https://via.placeholder.com/1000x1000', isFollow: false, collectionList: [{id: 5, imageUrl:'https://via.placeholder.com/1000x1000', isFundingNow: false}, {id: 6, imageUrl:'https://via.placeholder.com/1000x1000', isFundingNow: false}]},
-  {id: 4, name: 'Partimento', simpleDescription: 'Partimento 관련 브랜드 간단 소개글', logoImageUrl: 'https://via.placeholder.com/1000x1000',imageUrl: 'https://via.placeholder.com/1000x1000', isFollow: false, collectionList: [{id: 7, imageUrl:'https://via.placeholder.com/1000x1000', isFundingNow: false}, {id: 8, imageUrl:'https://via.placeholder.com/1000x1000', isFundingNow: false}]},
-  {id: 5, name: 'GBH', simpleDescription: 'GBH 관련 브랜드 간단 소개글', logoImageUrl: 'https://via.placeholder.com/1000x1000',imageUrl: 'https://via.placeholder.com/1000x1000', isFollow: false, collectionList: [{id: 9, imageUrl:'https://via.placeholder.com/1000x1000', isFundingNow: false}, {id: 10, imageUrl:'https://via.placeholder.com/1000x1000', isFundingNow: false}]},
-  {id: 6, name: 'Frontrow', simpleDescription: 'Frontrow 관련 브랜드 간단 소개글', logoImageUrl: 'https://via.placeholder.com/1000x1000',imageUrl: 'https://via.placeholder.com/1000x1000', isFollow: false, collectionList: [{id: 11, imageUrl:'https://via.placeholder.com/1000x1000', isFundingNow: false}, {id: 12, imageUrl:'https://via.placeholder.com/1000x1000', isFundingNow: false}]},
-  {id: 7, name: 'fureweb', simpleDescription: 'fureweb 관련 브랜드 간단 소개글', logoImageUrl: 'https://via.placeholder.com/1000x1000',imageUrl: 'https://via.placeholder.com/1000x1000', isFollow: false, collectionList: [{id: 13, imageUrl:'https://via.placeholder.com/1000x1000', isFundingNow: false}, {id: 14, imageUrl:'https://via.placeholder.com/1000x1000', isFundingNow: false}]},
-  {id: 8, name: 'bindus', simpleDescription: 'bindus 관련 브랜드 간단 소개글', logoImageUrl: 'https://via.placeholder.com/1000x1000',imageUrl: 'https://via.placeholder.com/1000x1000', isFollow: false, collectionList: [{id: 15, imageUrl:'https://via.placeholder.com/1000x1000', isFundingNow: false}, {id: 16, imageUrl:'https://via.placeholder.com/1000x1000', isFundingNow: false}]},
+  {
+    id: 1, name: 'Mother ground',
+    simple_description: '마더그라운드의 새 엘리먼트입니다. <br>까마귀烏오 WU Series',
+    logo_image_url: '/images/img-brandlogo-motherground-48.svg',
+    image_url:'/images/mother_ground.jpg',
+    is_follow: false,
+    latest_collection: {
+      id: 1,
+      is_funding_now: true,
+      image_list: ['/images/1.jpg', '/images/2.jpg']
+    },
+  },
+  {
+    id: 2, name: 'Rawrow',
+    simple_description: 'Rawrow 관련 브랜드 간단 소개글',
+    logo_image_url: '/images/img-brandlogo-rawrow-48.svg',
+    image_url:'https://via.placeholder.com/1000x1000',
+    is_follow: false,
+    latest_collection: {
+      id: 2,
+      is_funding_now: false,
+      image_list: ['/images/1.jpg', '/images/2.jpg']
+    },
+  },
+  {
+    id: 3, name: 'Hideout',
+    simple_description: 'Hideout 관련 브랜드 간단 소개글',
+    logo_image_url: '/images/img-brandlogo-hideout-48.svg', 
+    image_url:'https://via.placeholder.com/1000x1000',
+    is_follow: false,
+    latest_collection: {
+      id: 3,
+      is_funding_now: false,
+      image_list: ['/images/1.jpg', '/images/2.jpg']
+    },
+  },
+  {
+    id: 4, name: 'Partimento',
+    simple_description: 'Partimento 관련 브랜드 간단 소개글',
+    logo_image_url: 'https://via.placeholder.com/1000x1000',
+    image_url: 'https://via.placeholder.com/1000x1000',
+    is_follow: false,
+    latest_collection: {
+      id: 4,
+      is_funding_now: false,
+      image_list: ['/images/1.jpg', '/images/2.jpg']
+    },
+  },
+  {
+    id: 5, name: 'GBH',
+    simple_description: 'GBH 관련 브랜드 간단 소개글',
+    logo_image_url: 'https://via.placeholder.com/1000x1000',
+    image_url: 'https://via.placeholder.com/1000x1000',
+    is_follow: false,
+    latest_collection: {
+      id: 5,
+      is_funding_now: false,
+      image_list: ['/images/1.jpg', '/images/2.jpg']
+    },
+  },
+  {
+    id: 6, name: 'Frontrow',
+    simple_description: 'Frontrow 관련 브랜드 간단 소개글',
+    logo_image_url: 'https://via.placeholder.com/1000x1000',
+    image_url: 'https://via.placeholder.com/1000x1000',
+    is_follow: false,
+    latest_collection: {
+      id: 6,
+      is_funding_now: false,
+      image_list: ['/images/1.jpg', '/images/2.jpg']
+    },
+  },
+  {
+    id: 7, name: 'fureweb',
+    simple_description: 'fureweb 관련 브랜드 간단 소개글',
+    logo_image_url: 'https://via.placeholder.com/1000x1000',
+    image_url: 'https://via.placeholder.com/1000x1000',
+    is_follow: false,
+    latest_collection: {
+      id: 7,
+      is_funding_now: true,
+      image_list: ['/images/1.jpg', '/images/2.jpg']
+    },
+  },
+  {
+    id: 8, name: 'bindus',
+    simple_description: 'bindus 관련 브랜드 간단 소개글',
+    logo_image_url: 'https://via.placeholder.com/1000x1000',
+    image_url: 'https://via.placeholder.com/1000x1000',
+    is_follow: false,
+    latest_collection: {
+      id: 8,
+      is_funding_now: false,
+      image_list: ['/images/1.jpg', '/images/2.jpg']
+    },
+  },
 ]
 
 module.exports = {
@@ -17,8 +105,8 @@ module.exports = {
     if (req.param('listOnly')) {
       return res.json({brandList: brandList.map(brand => {
         const target = Object.assign({}, brand)
-        delete target.simpleDescription
-        delete target.collectionList
+        delete target.simple_description
+        delete target.collectionImageList
         return target
       })})
     }
